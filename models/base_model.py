3b9c094e68ff012551623cb89d14a5a4b8079d0b
quin-Amazin <ugochiamazin@gmail.com>
2022-09-03 17:38:14 -0700

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,10 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     format = "%Y-%m-%dT%H:%M:%S.%f"
-                    setattr(self, key, datetime.strptime(value, format))
+                    value = datetime.strptime(value, format)
                 elif key == "__class__":
                     continue
-                else:
-                    setattr(self, key, value)
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
