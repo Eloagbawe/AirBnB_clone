4f74f4d65ba5b714743dedc7c924314f52047092
Eloagbawe <eloagbawe@gmail.com>
2022-09-04 18:43:09 -0700

    update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -17,12 +17,12 @@ class BaseModel:
         """
 
         if len(kwargs) != 0:
-            if "__class__" in kwargs:
-                del kwargs["__class__"]
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     format = "%Y-%m-%dT%H:%M:%S.%f"
                     value = datetime.strptime(value, format)
+                elif key == '__class__':
+                    continue
                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
